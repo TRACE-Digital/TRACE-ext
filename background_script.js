3f088a50762b844458a93a668b213f023597524f
Jack McKernan <jmcker@outlook.com>
2021-02-11 15:19:30 -0500

    [UNTESTED] Add starting point for stripping CORS
    Co-authored-by: Cora Chan <chan207@purdue.edu>

--- background_script.js
+++ background_script.js
@@ -1,1 +1,23 @@
-// Put all the javascript code here, that you want to execute in background.
+var strip_cors = true;
+
+chrome.webRequest.onHeadersReceived.addListener(
+    function (details) {
+
+        console.log(details);
+
+        var headers = details.responseHeaders;
+        if (strip_cors) {
+            for (var i = 0; i < headers.length; i++) {
+                if (headers[i].name.toLowerCase() == 'access-control-allow-origin') {
+                    headers[i].value = '*';
+                }
+            }
+        }
+        return { responseHeaders: headers };
+    },
+    {
+        urls: ['<all_urls>'],
+        types: ['main_frame', 'sub_frame']//,'stylesheet','script','image','object','xmlhttprequest','other']
+    },
+    ['blocking', 'responseHeaders']
+);
