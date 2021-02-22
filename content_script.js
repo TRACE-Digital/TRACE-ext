6aa51830f822a6cdfcf9a82fc30d83d4e038ed38
Cora Chan <corachan055@gmail.com>
2021-02-22 17:38:53 -0500

    Grab fields from page

--- content_script.js
+++ content_script.js
@@ -1,1 +1,34 @@
 // Put all the javascript code here, that you want to execute after page load.
+
+window.addEventListener("submit", 
+    function(e) {
+        if (document.readyState === "complete") {
+            findUsernameField();
+        }
+        else {
+            window['onload'] = function () {
+                findUsernameField();
+            }
+        }
+    }, false
+);
+
+// Find the username for easy add to dashboard
+const findUsernameField = () => {
+    var inputs = document.getElementsByTagName('input');
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type.toLowerCase() == 'login') {
+            console.log(inputs[i].value);
+        }
+    }
+}
+
+// Not used, might be expanded later for password manager
+const findPasswordField = () => {
+    var inputs = document.getElementsByTagName('input');
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type.toLowerCase() == 'password') {
+            console.log(inputs[i].value);
+        }
+    }
+}
