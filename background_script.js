34366bc2bd313c0dc5434d5ec78035986560332a
Cora Chan <corachan055@gmail.com>
2021-03-03 00:36:25 -0500

    Add more CORS headers

--- background_script.js
+++ background_script.js
@@ -7,16 +7,32 @@ const valid_sites = new RegExp('http://localhost/*');
 chrome.webRequest.onHeadersReceived.addListener(
     function (details) {
         strip_cors = checkInitiator(details);
-        var access_control_header_index = -1;
+        var access_control_origin_index = -1;
+        var access_control_headers_index = -1;
+        var access_control_creds_index = -1;
         if (strip_cors) {
             for (var i = 0; i < details.responseHeaders.length; i++) {
                 if (details.responseHeaders[i].name.toLowerCase() == 'access-control-allow-origin') {
-                    access_control_header_index = i;
+                    access_control_origin_index = i;
                     details.responseHeaders[i].value = details.initiator;
                 }
+                if (details.responseHeaders[i].name.toLowerCase() == 'access-control-allow-headers') {
+                    access_control_headers_index = i;
+                    details.responseHeaders[i].value = 'Include';
+                }
+                if (details.responseHeaders[i].name.toLowerCase() == 'access-control-allow-credentials') {
+                    access_control_creds_index = i;
+                    details.responseHeaders[i].value = 'Expose';
+                }
             }
-            if (access_control_header_index == -1) {
+            if (access_control_origin_index == -1) {
                 details.responseHeaders.push({name: 'Access-Control-Allow-Origin', value: details.initiator})
+            }
+            if (access_control_headers_index == -1) {
+                details.responseHeaders.push({name: 'Access-Control-Allow-Headers', value: 'Include'})
+            }
+            if (access_control_creds_index == -1) {
+                details.responseHeaders.push({name: 'Access-Control-Allow-Credentials', value: 'Expose'})
             }
         }
         console.log(details);
