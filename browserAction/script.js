4fb40f7aa7845abcdaa897551c5a33cd28a793cd
Cora Chan <corachan055@gmail.com>
2021-03-02 21:38:50 -0500

    Extension popup

--- browserAction/script.js
+++ browserAction/script.js
@@ -1,1 +1,15 @@
-document.getElementById('myHeading').style.color = 'red'
+function openTab(tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+}
+
+document.getElementById('add-site').onclick = openTab("London");
+document.getElementById('pwnd').onclick = openTab("Paris")
