7d5558a27b05d55d3841e0fe4edb2f880db93855
cohenchris <cohen50@purdue.edu>
2021-03-03 14:40:41 -0500

    hooked into AWS API Gateway, bypassing CORS problem

--- haveibeenpwnd/hibp.js
+++ haveibeenpwnd/hibp.js
@@ -5,25 +5,23 @@
  * Return results
  */
 
+/**
+ * https://stackoverflow.com/questions/8739605/getelementbyid-returns-null
+ */
+(function (window, document, undefined) {
+  // code that should be taken care of right away
 
- /**
-  * https://stackoverflow.com/questions/8739605/getelementbyid-returns-null
-  */
-(function(window, document, undefined){
+  window.onload = init;
 
-    // code that should be taken care of right away
-
-    window.onload = init;
-    
-      function init(){
-        document.getElementById('entry').addEventListener('input', validateInput);
-        document.getElementById('email').addEventListener('click', validateInput);
-        document.getElementById('password').addEventListener('click', validateInput);
-        document.getElementById('search').addEventListener('click', haveIBeenPwnd);
-      }
-    
+  function init() {
+    document.getElementById("entry").addEventListener("input", validateInput);
+    document.getElementById("email").addEventListener("click", validateInput);
+    document
+      .getElementById("password")
+      .addEventListener("click", validateInput);
+    document.getElementById("search").addEventListener("click", haveIBeenPwnd);
+  }
 })(window, document, undefined);
-    
 
 /**
  * Controls submit button being disabled if form fields aren't filled out correctly
@@ -32,67 +30,109 @@
  *      One of the radio buttons must be selected
  */
 const validateInput = () => {
-    if ($('#entry').val() == '' || $('#entry').val() == undefined) {
-        $('#search').attr("disabled", true)
-    }
-    else if (($('#email').prop("checked") == $('#password').prop("checked"))) {
-        $('#search').attr("disabled", true)
-    }
-    else {
-        $('#search').attr("disabled", false)
-    }
-}
+  if ($("#entry").val() == "" || $("#entry").val() == undefined) {
+    $("#search").attr("disabled", true);
+  } else if ($("#email").prop("checked") == $("#password").prop("checked")) {
+    $("#search").attr("disabled", true);
+  } else {
+    $("#search").attr("disabled", false);
+  }
+};
 
 /**
  * Depending on submitted data, check if email or password has been pwnd and display the data
- * @param {*} event 
+ * @param {*} event
  */
 const haveIBeenPwnd = async (event) => {
-    const entry = $('#entry').val()
-    const isEmail = $('#email').prop("checked")
-    const isPassword = $('#password').prop("checked")
+  const entry = $("#entry").val();
+  const isEmail = $("#email").prop("checked");
+  const isPassword = $("#password").prop("checked");
 
-    if (isEmail) {
-        await hasEmailBeenPwnd(entry)
-    }
-    else {  // isPassword
-        await hasPasswordBeenPwnd(entry)
-    }
-}
-
-
+  if (isEmail) {
+    await hasEmailBeenPwnd(entry);
+  } else {
+    // isPassword
+    await hasPasswordBeenPwnd(entry);
+  }
+};
 
 /**
  * Actual HIBP Requests
  */
 
 const request_headers = {
-    "hibp-api-key": "",         // TODO: don't commit this
-}
+  "hibp-api-key": "", // TODO: don't commit this
+};
 
 const hasEmailBeenPwnd = async (email) => {
-    const service = "breachedaccount"
-    const url = `https://haveibeenpwned.com/api/v3/${service}/${email}`
+  const aws_api_gateway = "";                                     
 
-    const emailData = await fetch(url, { headers: request_headers })
-                                .catch( (error) => {
-                                    console.log(error)
-                                })
+  const url = `${aws_api_gateway}/breachedaccount/${email}`;
 
-    console.log(emailData)
+  const emailData = await fetch(url, { headers: request_headers })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((error) => {
+      console.log(error);
+      return {};
+    });
 
-    // TODO: maybe call detailed view?
-    // GET https://haveibeenpwned.com/api/v3/breach/{name}
-    // idea: give list of websites, and then link to deep link (ex:  https://haveibeenpwned.com/account/test@example.com )
-}
+  console.log(emailData);
+
+  // TODO: maybe call detailed view?
+  // GET https://haveibeenpwned.com/api/v3/breach/{name}
+  // idea: give list of websites, and then link to deep link (ex:  https://haveibeenpwned.com/account/test@example.com )
+};
 
 const hasPasswordBeenPwnd = async (password) => {
-    const hashedPassword = password       // TODO: compute SHA1
-    const url = `https://api.pwnedpasswords.com/range/${hashedPassword.substring(0,5)}`
-    const passwordData = await fetch(url, { headers: request_headers })
-                                    .catch( (error) => {
-                                        console.log(error)
-                                    })
+  const aws_api_gateway = "";
 
-    console.log(passwordData)
+  const hashedPassword = (await sha1(password)).toString();
+  console.log(hashedPassword.substring(0,5))
+  const url = `${aws_api_gateway}/range/${hashedPassword.substring(0, 5)}`;
+
+  const passwordData = await fetch(url, { headers: request_headers })
+    .then((response) => {
+      return response.text();
+    })
+    .catch((error) => {
+      console.log(error);
+      return "";
+    });
+
+  const passwords = passwordData.split("\r\n")
+  
+  let numTimesPwnd = 0
+
+  for (password of passwords) {
+    console.log(password)
+    console.log(parseInt(password.split(":")[1]))
+    numTimesPwnd += parseInt(password.split(":")[1])
+  }
+
+  console.log(`You've been Pwnd ${numTimesPwnd} times!`)    // TODO: something's not quite right...
+
+};
+
+
+
+// SHA-1 Implementation
+//https://github.com/jamiebuilds/havetheybeenpwned/blob/master/lib/sha1-browser.js
+("use strict");
+
+function bufferToHex(buffer) {
+  let view = new DataView(buffer);
+  let hexCodes = "";
+  for (let i = 0; i < view.byteLength; i += 4) {
+    hexCodes += view.getUint32(i).toString(16).padStart(8, "0");
+  }
+  return hexCodes;
+}
+
+function sha1(str) {
+  let buffer = new TextEncoder().encode(str);
+  return crypto.subtle
+    .digest("SHA-1", buffer)
+    .then((hash) => bufferToHex(hash));
 }
