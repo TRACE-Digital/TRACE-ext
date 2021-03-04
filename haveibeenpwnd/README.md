3b82ead92c2d4ee7c1d3a723960560a3f2ba3c85
cohenchris <cohen50@purdue.edu>
2021-03-03 19:42:38 -0500

    finished skeleton, need to make it actually look good now

--- haveibeenpwnd/README.md
+++ haveibeenpwnd/README.md
@@ -1,8 +1,8 @@
 # TODO
 - [x] Add pastebin breach functionality
 - [x] Make AWS use hibp-api-key by default, instead of sending it client-side
-- [ ] Print results
-- [ ] CSS for widget color green if not compromised
-- [ ] CSS for widget color red if compromised
+- [x] Print results
+- [x] CSS for widget color green if not compromised
+- [x] CSS for widget color red if compromised
 - [ ] Make it look pretty! ((bootstrap?)
     - [ ] Fade animations for green/red?
--- haveibeenpwnd/hibp.js
+++ haveibeenpwnd/hibp.js
@@ -16,6 +16,22 @@
   }
 })(window, document, undefined);
 
+
+/*
+          SMALL HELPERS
+*/
+
+
+
+const showData = (msg) => {
+  $(".responseBody").html(msg);
+};
+
+const AWSApiGateway =
+  "https://s7kbw14q30.execute-api.us-east-2.amazonaws.com/production/";
+
+
+
 /**
  * Controls submit button being disabled if form fields aren't filled out correctly
  * Requirements:
@@ -31,6 +47,7 @@ const validateInput = () => {
     $("#search").attr("disabled", false);
   }
 };
+
 
 /**
  * Depending on submitted data, check if email or password has been pwnd and display the data
@@ -49,14 +66,13 @@ const haveIBeenPwnd = async (event) => {
   }
 };
 
+
 /**
- * Actual HIBP Requests
+ * Checks the HaveIBeenPwnd API for how many times a given email has been exposed in a data breach.
+ * @param {*} email 
  */
-
-const AWSApiGateway = "https://s7kbw14q30.execute-api.us-east-2.amazonaws.com/production/";
-
 const hasEmailBeenPwnd = async (email) => {
-  showData(`Checking known breaches and pastebins for ${email}...`)
+  showData(`Checking known breaches and pastebins for ${email}...`);
 
   /////////////// Email check! ///////////////
   const emailUrl = `${AWSApiGateway}/breachedaccount/${email}`;
@@ -70,37 +86,39 @@ const hasEmailBeenPwnd = async (email) => {
       return [];
     });
 
-
   /////////////// Pastebin check! ///////////////
   const pastebinUrl = `${AWSApiGateway}/pasteaccount/${email}`;
 
   const pastebinData = await fetch(pastebinUrl)
-  .then((response) => {
-    console.log(response)
-    return response.json();
-  })
-  .catch((error) => {
-    console.log(error);
-    return [];
-  });
-
+    .then((response) => {
+      console.log(response);
+      return response.json();
+    })
+    .catch((error) => {
+      console.log(error);
+      return [];
+    });
 
   /////////////// Show final results on screen ///////////////
-  let emailResults = await buildEmailResults(emailData, pastebinData)
-  showData(emailResults)
+  let emailResults = await buildEmailResults(emailData, pastebinData);
+  showData(emailResults);
 
-  // TODO: maybe call detailed view?  https://haveibeenpwned.com/api/v3/pasteaccount
-  // GET https://haveibeenpwned.com/api/v3/breach/{name}
+  // TODO: maybe call detailed view?
+  // GET https://haveibeenpwned.com/api/v3/breach/{name}  '691a5b7a373d4f33b8c6bdfae0c4f4af'
   // idea: give list of websites, and then link to deep link (ex:  https://haveibeenpwned.com/account/test@example.com )
 };
 
 
+/**
+ * Checks the HaveIBeenPwnd API for how many times a given password has been exposed in a data breach
+ * @param {*} password 
+ */
 const hasPasswordBeenPwnd = async (password) => {
-  showData("Checking known breaches for your password...")
+  showData("Checking known breaches for your password...");
 
   // This API endpoint requires the first 5 characters of the SHA1 hash
-  const hashedPassword = (await sha1(password)).toString();
-  console.log(hashedPassword.substring(0,5))
+  const hashedPassword = (await sha1(password)).toString().toUpperCase();
+  console.log(hashedPassword.substring(0, 5));
 
   const url = `${AWSApiGateway}/range/${hashedPassword.substring(0, 5)}`;
 
@@ -113,45 +131,84 @@ const hasPasswordBeenPwnd = async (password) => {
       return "";
     });
 
-  const passwords = passwordData.split("\r\n")
-  
-  let numTimesPwnd = 0
+  const passwords = passwordData.split("\r\n");
+
+  let numTimesPwnd = 0;
 
   for (password of passwords) {
-    console.log(password)
-    console.log(parseInt(password.split(":")[1]))
-    numTimesPwnd += parseInt(password.split(":")[1])
+    const hash = password.split(":")[0];
+    const num = password.split(":")[1];
+    if (hashedPassword.substring(5).localeCompare(hash) == 0) {
+      numTimesPwnd += parseInt(num);
+    }
   }
 
-  showData(`You've been Pwnd ${numTimesPwnd} times!`)    // TODO: something's not quite right with the number...
+  if (numTimesPwnd == 0) {
+    showData("This password has never been exposed!");
+    $("body").addClass("notPwnd");
+  } else {
+    showData(`This password been exposed ${numTimesPwnd} times.`);
+    $("body").addClass("pwnd");
+  }
 };
 
 
-const showData = (msg) => {
-  $('.responseBody').html(msg);
-}
-
+/**
+ * Takes in the data received from HaveIBeenPwnd API and formats it in a visually pleasing way
+ * @param {*} emailData 
+ * @param {*} pastebinData 
+ */
 const buildEmailResults = async (emailData, pastebinData) => {
-  let emailResults = ""
+  let pwnd = false;
+  let emailResults = "";
 
-  if (length(emailData) != 0) {
-    emailResults += "Yikes! Your email has been breached on the following websites:\n"
+  if (emailData.length != 0) {
+    pwnd = true;
+    emailResults +=
+      "<h3>Yikes! Your email has been exposed on the following websites:</h3>\n";
+    emailResults += "<ul>";
     for (email of emailData) {
-      let siteName = JSON.stringify(email.Name)
-      emailResults += siteName + "\n"
+      let siteName = JSON.stringify(email.Name);
+      siteName = siteName.substring(1, siteName.length - 1);
+
+      // emailResults += `<li><a href="https://haveibeenpwned.com/api/v3/breach/${siteName}">${siteName}</a></li>\n`;   // TODO: keep this?
+      emailResults += `<li>${siteName}</li>\n`;
     }
+    emailResults += "</ul>\n\n<br/>\n";
   }
 
-  if (length(pastebinData) != 0) {
-    emailResults += "\n\nand the following pastebin leaks:"
+  if (pastebinData.length != 0) {
+    pwnd = true;
+    emailResults +=
+      emailData.length != 0
+        ? "<h3>...and the following pastebin leaks:</h3>\n"
+        : "<h3>Yikes! Your email has been exposed in the following pastebin leaks:</h3>";
+
+    emailResults += "<ul>";
     for (pastebin of pastebinData) {
-      let pastebinName = JSON.stringify(pastebin)
-      emailResults += pastebinName + "\n"
+      const source = JSON.stringify(pastebin.Source);
+      const id = JSON.stringify(pastebin.Id);
+      const title = JSON.stringify(pastebin.Title);
+      const date = JSON.stringify(pastebin.Date);
+      const emailCount = JSON.stringify(pastebin.EmailCount);
+      emailResults += `<li> <p><strong>Source:</strong>     ${source}</p>
+                            <p><strong>Id:</strong>         ${id}</p>
+                            <p><strong>Title:</strong>      ${title}</p>
+                            <p><strong>Date:</strong>       ${date}</p>
+                            <p><strong>EmailCount:</strong> ${emailCount}</p>
+                      </li>\n`;
     }
+    emailResults += "</ul>\n\n<br/><br/>";
   }
 
-  return emailResults
-}
+  pwnd ? $("body").addClass("pwnd") : $("body").addClass("notPwnd");
+
+  return emailResults;
+};
+
+
+
+
 
 
 // SHA-1 Implementation
