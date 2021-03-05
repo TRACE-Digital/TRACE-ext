3f34c4ce9d750976e53881f72d1bc93ae38a5323
Cora Chan <corachan055@gmail.com>
2021-03-04 19:20:45 -0500

    Fixed CORS redirect error

--- background_script.js
+++ background_script.js
@@ -4,35 +4,34 @@ var strip_cors = true;
 // TODO: Add our site when deployed
 const valid_sites = new RegExp('http://localhost/*');
 
+// Keep track of redirected urls - THIS IS A HACK FOR NOW
+var redirected_urls = [];
+
 chrome.webRequest.onHeadersReceived.addListener(
     function (details) {
         strip_cors = checkInitiator(details);
-        var access_control_origin_index = -1;
-        var access_control_headers_index = -1;
-        var access_control_creds_index = -1;
+
         if (strip_cors) {
-            for (var i = 0; i < details.responseHeaders.length; i++) {
-                if (details.responseHeaders[i].name.toLowerCase() == 'access-control-allow-origin') {
-                    access_control_origin_index = i;
-                    details.responseHeaders[i].value = details.initiator;
-                }
-                if (details.responseHeaders[i].name.toLowerCase() == 'access-control-allow-headers') {
-                    access_control_headers_index = i;
-                    details.responseHeaders[i].value = 'Include';
-                }
-                if (details.responseHeaders[i].name.toLowerCase() == 'access-control-allow-credentials') {
-                    access_control_creds_index = i;
-                    details.responseHeaders[i].value = 'Expose';
-                }
+            console.log(details.url);
+            // Check if redirected url
+            if (redirected_urls.includes(details.url)) {
+                console.log("This is a redirected URL");
+                // Set access-control-allow-origin header to wildcard for redirected urls
+                // because initial initiator is immutable but origin will change causing CORS error
+                changeHeaders(details, '*', 'Include', 'Expose');
+                // Remove this url from the list of redirected urls
+                redirected_urls.splice(redirected_urls.indexOf(details.url), 1);
             }
-            if (access_control_origin_index == -1) {
-                details.responseHeaders.push({name: 'Access-Control-Allow-Origin', value: details.initiator})
-            }
-            if (access_control_headers_index == -1) {
-                details.responseHeaders.push({name: 'Access-Control-Allow-Headers', value: 'Include'})
-            }
-            if (access_control_creds_index == -1) {
-                details.responseHeaders.push({name: 'Access-Control-Allow-Credentials', value: 'Expose'})
+            else {
+                // Check status code for redirects
+                if (details.statusCode < 400 && details.statusCode >= 300) {
+                    console.log("This site is redirecting to another URL");
+                    var location = getLocation(details);
+                    if (location) {
+                        redirected_urls.push(location);
+                    }
+                }
+                changeHeaders(details, details.initiator, 'Include', 'Expose');
             }
         }
         console.log(details);
@@ -45,6 +44,50 @@ chrome.webRequest.onHeadersReceived.addListener(
     ['blocking', 'responseHeaders', 'extraHeaders']
     // https://developer.chrome.com/docs/extensions/reference/webRequest/#life-cycle-of-requests
 );
+
+// Change header values if they exist, add them if not
+function changeHeaders(details, allow_origin_value, allow_headers_value, allow_credentials_value) {
+
+    var access_control_origin_index = -1;
+    var access_control_headers_index = -1;
+    var access_control_creds_index = -1;
+
+    // Modify headers to allow CORS
+    for (var i = 0; i < details.responseHeaders.length; i++) {
+        if (details.responseHeaders[i].name.toLowerCase() == 'access-control-allow-origin') {
+            access_control_origin_index = i;
+            details.responseHeaders[i].value = allow_origin_value;
+        }
+        if (details.responseHeaders[i].name.toLowerCase() == 'access-control-allow-headers') {
+            access_control_headers_index = i;
+            details.responseHeaders[i].value = allow_headers_value;
+        }
+        if (details.responseHeaders[i].name.toLowerCase() == 'access-control-allow-credentials') {
+            access_control_creds_index = i;
+            details.responseHeaders[i].value = allow_credentials_value;
+        }
+    }
+    // Add headers if they don't exist
+    if (access_control_origin_index == -1) {
+        details.responseHeaders.push({name: 'Access-Control-Allow-Origin', value: allow_origin_value})
+    }
+    if (access_control_headers_index == -1) {
+        details.responseHeaders.push({name: 'Access-Control-Allow-Headers', value: allow_headers_value})
+    }
+    if (access_control_creds_index == -1) {
+        details.responseHeaders.push({name: 'Access-Control-Allow-Credentials', value: allow_credentials_value})
+    }
+}
+
+// Find new redirected url
+function getLocation(details) {
+    for (var i = 0; i < details.responseHeaders.length; i++) {
+        if (details.responseHeaders[i].name.toLowerCase() == 'location') {
+            return details.responseHeaders[i].value;
+        }
+    }
+    return null;
+}
 
 // Check if request came from a valid site
 function checkInitiator(details) {
